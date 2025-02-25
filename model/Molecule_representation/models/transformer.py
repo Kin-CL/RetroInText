c13b814e27132fb8b15a17c40b31044d33a7d84b
Kin-CL <83826791+Kin-CL@users.noreply.github.com>
2025-02-25 20:38:27 +0800

    Update transformer.py

--- model/Molecule_representation/models/transformer.py
+++ model/Molecule_representation/models/transformer.py
@@ -8,8 +8,8 @@ from torch import nn
 import torch.nn.functional as F
 from torch.nn import TransformerEncoderLayer
 
-from Molecule_representation.commons.mol_encoder import AtomEncoder, BondEncoder
-from Molecule_representation.models.base_layers import MLP
+from model.Molecule_representation.commons.mol_encoder import AtomEncoder, BondEncoder
+from model.Molecule_representation.models.base_layers import MLP
 
 EPS = 1e-5
 
