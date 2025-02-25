df0ba45b5729c57a2be962caf019d6735469a133
Kin-CL <83826791+Kin-CL@users.noreply.github.com>
2025-02-25 20:34:52 +0800

    Update egnn.py

--- model/Molecule_representation/models/egnn.py
+++ model/Molecule_representation/models/egnn.py
@@ -6,8 +6,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import dgl.function as fn
 
-from Molecule_representation.commons.utils import fourier_encode_dist
-from Molecule_representation.models.base_layers import MLP
+from model.Molecule_representation.commons.utils import fourier_encode_dist
+from model.Molecule_representation.models.base_layers import MLP
 
 
 class EGNN(nn.Module):
