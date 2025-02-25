a01da87e16169f0549163d6ffba28a674997bdb9
Kin-CL <83826791+Kin-CL@users.noreply.github.com>
2025-02-25 20:37:59 +0800

    Update pna_transformer.py

--- model/Molecule_representation/models/pna_transformer.py
+++ model/Molecule_representation/models/pna_transformer.py
@@ -8,10 +8,10 @@ from torch import nn
 import torch.nn.functional as F
 from torch.nn import TransformerEncoderLayer
 
-from Molecule_representation.commons.mol_encoder import AtomEncoder, BondEncoder
-from Molecule_representation.models.base_layers import MLP
-from Molecule_representation.models.pna import PNALayer
-from Molecule_representation.models.pna_original import PNASimpleLayer
+from model.Molecule_representation.commons.mol_encoder import AtomEncoder, BondEncoder
+from model.Molecule_representation.models.base_layers import MLP
+from model.Molecule_representation.models.pna import PNALayer
+from model.Molecule_representation.models.pna_original import PNASimpleLayer
 
 EPS = 1e-5
 
