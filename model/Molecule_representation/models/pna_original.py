085ccba914f9997166393b8b37a91772469cb14a
Kin-CL <83826791+Kin-CL@users.noreply.github.com>
2025-02-25 17:37:41 +0800

    Update pna_original.py

--- model/Molecule_representation/models/pna_original.py
+++ model/Molecule_representation/models/pna_original.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import dgl.function as fn
 
-from RetroT.model.Molecule_representation.commons.mol_encoder import AtomEncoder, BondEncoder
+from Molecule_representation.commons.mol_encoder import AtomEncoder, BondEncoder
 
 EPS = 1e-5
 import numpy as np
-from RetroT.model.Molecule_representation.models.base_layers import MLP, MLPReadout
+from Molecule_representation.models.base_layers import MLP, MLPReadout
 
 """
     code from:
